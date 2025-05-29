f18513e00d1aaffdd6ac4bce3b41d25e5295b33b
Diego M. Rodrigues <diegomr86@gmail.com>
2025-05-29 15:13:36 -0300

    Add logging to Mongoose plugin and connection utility for better debugging

--- plugins/mongoose.ts
+++ plugins/mongoose.ts
@@ -2,7 +2,9 @@
 import { connectToDatabase } from "~/server/utils/mongoose";
 
 export default defineNuxtPlugin(async () => {
+  console.log("Mongoose plugin loaded");
   if (import.meta.server) {
+    console.log("Calling connectToDatabase");
     await connectToDatabase();
   }
 });
--- server/utils/mongoose.ts
+++ server/utils/mongoose.ts
@@ -3,19 +3,13 @@ import mongoose from "mongoose";
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/teia";
 
 export const connectToDatabase = async () => {
+  console.log("Connecting to MongoDB");
+  console.log("MONGO_URI", MONGO_URI);
+  console.log("mongoose.connection.readyState", mongoose.connection.readyState);
+
   if (mongoose.connection.readyState === 0) {
     try {
-      await mongoose.connect(MONGO_URI, {
-        maxPoolSize: 10,
-        minPoolSize: 5,
-        serverSelectionTimeoutMS: 30000,
-        socketTimeoutMS: 45000,
-        family: 4,
-        retryWrites: true,
-        retryReads: true,
-        w: 'majority',
-        wtimeoutMS: 2500
-      });
+      await mongoose.connect(MONGO_URI);
       console.log("Connected to MongoDB");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
