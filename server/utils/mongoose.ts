4541d10ac0c97dd41ddae2bd43e6d03c34412837
Diego M. Rodrigues <diegomr86@gmail.com>
2025-05-29 14:34:24 -0300

    Refine MongoDB connection settings by adding write concern and timeout configurations to enhance reliability and performance during database interactions.

--- server/utils/mongoose.ts
+++ server/utils/mongoose.ts
@@ -6,13 +6,15 @@ export const connectToDatabase = async () => {
   if (mongoose.connection.readyState === 0) {
     try {
       await mongoose.connect(MONGO_URI, {
-        serverSelectionTimeoutMS: 30000, // Timeout for server selection
-        socketTimeoutMS: 45000, // Timeout for socket operations
-        connectTimeoutMS: 30000, // Timeout for initial connection
-        maxPoolSize: 10, // Maximum number of connections in the pool
-        minPoolSize: 5, // Minimum number of connections in the pool
+        maxPoolSize: 10,
+        minPoolSize: 5,
+        serverSelectionTimeoutMS: 30000,
+        socketTimeoutMS: 45000,
+        family: 4,
         retryWrites: true,
         retryReads: true,
+        w: 'majority',
+        wtimeoutMS: 2500
       });
       console.log("Connected to MongoDB");
     } catch (error) {
